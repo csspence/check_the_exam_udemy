d65de7827a5dd98abaaf83b612e4e7b95f594edc
Crew Spence <csspence123@gmail.com>
2020-07-20 05:29:50 -0500

    Wrote working solution.

--- check_the_exam_udemy.js
+++ check_the_exam_udemy.js
@@ -13,6 +13,17 @@ checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
 checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 */
 
-function checkExam(array1, array2) {
+const checkExam = (array1, array2) => {
+  let sum = 0;
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] === array2[i]) {
+      sum += 4;
+    } else if(array2[i] === '') {
+      continue;
+    } else {
+      sum -= 1;
+    }
+  }
 
+  return sum < 0 ? 0 : sum;
 }
